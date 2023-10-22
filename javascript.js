d3246c4463ba2d4cd97410b42d5cb9c07c2d3ce8
Grelgn <grelgn@hotmail.com>
2023-10-22 06:27:12 +0300

    Make displaying books a function

--- javascript.js
+++ javascript.js
@@ -11,11 +11,17 @@ function addBookToLibrary(name, author, pages, read) {
     myLibrary.push(new Book(name, author, pages, read));
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'not read');
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'Not read');
 
 const books = document.querySelector(".books");
 
-const book = document.createElement("div");
-book.classList.add("book");
-book.innerHTML = `<h3>${myLibrary[0].name}</h3> ${myLibrary[0].author} <br><br> ${myLibrary[0].pages} pages <br><br> ${myLibrary[0].read}`;
-books.appendChild(book);
+function displayBooks() {
+    myLibrary.forEach((element) => {
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `<h3>${element.name}</h3> ${element.author} <br><br> ${element.pages} pages <br><br> ${element.read}`;
+        books.appendChild(book);
+    });
+}
+
+displayBooks();
