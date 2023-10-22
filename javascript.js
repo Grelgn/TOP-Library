9136698c7334ef26830cafe7bd351e9cb438b4b8
Grelgn <grelgn@hotmail.com>
2023-10-22 06:15:27 +0300

    Add logic for displaying books in the library

--- javascript.js
+++ javascript.js
@@ -7,9 +7,15 @@ function Book(name, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    myLibrary.push(new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not read'));
+function addBookToLibrary(name, author, pages, read) {
+    myLibrary.push(new Book(name, author, pages, read));
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'not read');
+
+const books = document.querySelector(".books");
+
+const book = document.createElement("div");
+book.classList.add("book");
+book.innerHTML = `<h3>${myLibrary[0].name}</h3> ${myLibrary[0].author} <br><br> ${myLibrary[0].pages} pages <br><br> ${myLibrary[0].read}`;
+books.appendChild(book);
