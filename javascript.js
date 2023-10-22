19233e6ebed3093a79a68e96d0be10f0f46473d8
Grelgn <grelgn@hotmail.com>
2023-10-22 07:36:11 +0300

    Add the ability to add new books with a form

--- javascript.js
+++ javascript.js
@@ -16,6 +16,7 @@ addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'Not read');
 const books = document.querySelector(".books");
 
 function displayBooks() {
+    books.innerHTML = '';
     myLibrary.forEach((element) => {
         const book = document.createElement("div");
         book.classList.add("book");
@@ -25,3 +26,28 @@ function displayBooks() {
 }
 
 displayBooks();
+
+const dialog = document.querySelector("dialog");
+const add = document.querySelector(".add");
+const cancel = document.querySelector(".cancel");
+const addBook = document.querySelector(".add-book");
+
+add.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+cancel.addEventListener("click", () => {
+    dialog.close();
+});
+
+addBook.addEventListener("click", () => {
+    const formName = document.querySelector("#name").value;
+    const formAuthor = document.querySelector("#author").value;
+    const formPages = document.querySelector("#pages").value;
+    let formRead = document.querySelector("#read").checked;
+
+    formRead ? formRead = "Read" : formRead = "Not read";
+
+    addBookToLibrary(formName, formAuthor, formPages, formRead)
+    displayBooks();
+});
